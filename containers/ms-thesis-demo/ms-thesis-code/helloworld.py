489cf10742bafe2abb53fda77866bb1cb92f810c
Oğuz Arslan <53544274+Boysle@users.noreply.github.com>
2024-05-10 17:02:39 +0300

    Update helloworld.py

--- containers/ms-thesis-demo/ms-thesis-code/helloworld.py
+++ containers/ms-thesis-demo/ms-thesis-code/helloworld.py
@@ -1,5 +1,6 @@
 import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument("name", type = str, default = "oguz")
+args = parser.parse_args()
 name = args.name
 print("Hello " + name + "!")
