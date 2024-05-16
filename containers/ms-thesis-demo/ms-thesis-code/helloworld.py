26231be3ed5f97e322ab5f3eb9ae6c1608166e0e
Ufuk <ufukarslan99@gmail.com>
2024-05-16 17:05:32 +0300

    changed helloworld on pc

--- containers/ms-thesis-demo/ms-thesis-code/helloworld.py
+++ containers/ms-thesis-demo/ms-thesis-code/helloworld.py
@@ -1,6 +1,6 @@
 import argparse
 parser = argparse.ArgumentParser()
-parser.add_argument("--name", type = str, default = "oguz")
+parser.add_argument("--name", type = str, default = "oguzarslan")
 args = parser.parse_args()
 name = args.name
 print("Hello " + name + "!")
